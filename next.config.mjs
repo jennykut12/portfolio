666e8b42547c327dff9c10402fbb55dfe9450174
jennykut12 <jennykut.dj@gmail.com>
2024-03-07 23:57:30 +0100

    created the services screen

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,17 @@
 
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+    webpack: (config, { isServer }) => {
+      // Exclude .docx files from being processed by webpack
+      config.module.rules.push({
+        test: /\.docx$/,
+        use: 'ignore-loader', // or any other loader you prefer
+      });
+      
+      return config;
+    },
+  };  
+
 export default nextConfig;
